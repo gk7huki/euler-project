9a92695ca77f8272c52da9e597255703abd5aad6
huki <gk7huki@gmail.com>
2017-08-07 15:20:41 +0530

    fix solution to problem 3 (#1)
    Fixes #1.

--- problem3.js
+++ problem3.js
@@ -1,13 +1,13 @@
 var n = 600851475143;
 var largest = 0;
 
-if (n%2 === 0) {
+while (n%2 === 0) {
   largest = 2;
   n /= 2;
 }
 
 for (var i=3; i<=Math.sqrt(n); i+=2) {
-  if (n%i === 0) {
+  while (n%i === 0) {
     largest = i;
     n /= i;
   }
